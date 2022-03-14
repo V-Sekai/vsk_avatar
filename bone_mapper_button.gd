281cff3d96f480bfb1f6d83af584468918816019
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-14 09:48:11 -0700

    Rect names changed.

--- bone_mapper_button.gd
+++ bone_mapper_button.gd
@@ -7,10 +7,10 @@ var clear_bone_button: Button = null
 func _init():
 	select_bone_button = Button.new()
 	add_child(select_bone_button)
-	select_bone_button.rect_size = Vector2()
+	select_bone_button. = Vector2()
 	select_bone_button.size_flags_horizontal = SIZE_EXPAND_FILL
 	
 	clear_bone_button = Button.new()
-	clear_bone_button.rect_size = Vector2()
+	clear_bone_button. = Vector2()
 	add_child(clear_bone_button)
 
